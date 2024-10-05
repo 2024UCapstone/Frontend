4544895c9ea577bf5a4e4028c0239967a92c0c4e
jsum01 <djadjawltjr@gmail.com>
2024-10-05 20:59:30 +0900

    Update SearchBar: 검색창 초안

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,11 +5,13 @@ import MapComponent from "components/MapComponent/MapComponent";
 import Footer from "components/Footer/Footer";
 import ViewBusComponent from "components/ViewBusComponent/ViewBusComponent";
 import "./HomePage.css";
+import SearchBar from "pages/SearchBar/SearchBar";
 
 function HomePage() {
 
   return (
     <div className="HomePage">
+      <SearchBar />
       <KakaoMap />
       <ViewBusComponent />
       <Footer />
