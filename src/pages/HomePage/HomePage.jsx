1eb4c0804eb48e6f90aa926a3dc0007f5d3245ae
taehun2123 <pyoneng_@naver.com>
2024-10-13 18:16:02 +0900

    Update : 내 정류장 재작업 ; 엄지석 정신체리자

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,13 +8,19 @@ import StationPanel from "../../components/StationPanel/StationPanel";
 import SearchStationModal from "../../components/SearchStationModal/SearchStationModal";
 import { useModalState } from "store/UseModalStore";
 import MyInfoModal from "components/MyInfoModal/MyInfoModal";
+import useFetchData from "hooks/useFetchData";
+import usePostData from "hooks/usePostData";
+import useDeleteData from "hooks/useDeleteData";
+import LoadingPage from "pages/LoadingPage/LoadingPage";
 
 function HomePage() {
   const navigate = useNavigate();
   const location = useLocation();
   const [isSearchStationModalOpen, setIsSearchStationModalOpen] = useState(false);
-  const [stations, setStations] = useState([]);
-  const [favoriteStations, setFavoriteStations] = useState([]);
+  const { data: myStationData, load: myStationLoad, error: myStationError, fetchData: myStationFetchData } = useFetchData("http://devse.gonetis.com:12599/api/user/my-station");
+  const { postData: myStationPostData } = usePostData("http://devse.gonetis.com:12599/api/user/my-station");
+  const { deleteData: stationDel } = useDeleteData("http://devse.gonetis.com:12599/api/user/my-station");
+
   const { modalName, isModal } = useModalState();
 
   useEffect(() => {
@@ -28,9 +34,28 @@ function HomePage() {
     // 여기에 stations와 favoriteStations를 가져오는 로직 추가
   }, [location, navigate]);
 
-  const toggleFavorite = (stationId) => {
-    // 즐겨찾기 토글 로직 구현
+  useEffect(()=>{
+    myStationFetchData();
+  }, [])
+
+  const toggleFavorite = async (id) => {
+    try {
+      if (myStationData && myStationData.data?.find((item) => item.id === id)) {
+        await stationDel(id);
+        alert("정류장이 즐겨찾기에서 제거되었습니다.");
+      } else {
+        await myStationPostData({ stationId: id });
+        alert("정류장이 즐겨찾기에 추가었습니다.");
+      }
+      // 즐겨찾기 목록 갱신
+      myStationFetchData();
+    } catch (error) {
+      alert("작업 중 오류가 발생했습니다.");
+    }
   };
+
+  if (myStationLoad) return <LoadingPage/>;
+  if (myStationError) return <div>에러가 발생했습니다.</div>;
 
   return (
     <div className={styles.body}>
@@ -38,15 +63,13 @@ function HomePage() {
       <div className={styles.mapView}><MapView /></div>
       <StationPanel
         openSearchStationModal={() => setIsSearchStationModalOpen(true)}
-        stations={stations}
-        favoriteStations={favoriteStations}
+        favoriteStations={myStationData.data}
         toggleFavorite={toggleFavorite}
       />
       <SearchStationModal
         isOpen={isSearchStationModalOpen}
         toggleModal={() => setIsSearchStationModalOpen(false)}
-        stations={stations}
-        favoriteStations={favoriteStations}
+        favoriteStations={myStationData.data}
         toggleFavorite={toggleFavorite}
       />
       {isModal && modalName === "myInfoModal" && <MyInfoModal/>}
